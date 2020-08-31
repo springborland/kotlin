5608a19759c19b6e202994c6cdc83a1cb7660667
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2020-08-31 12:09:43 +0200

    Implement JVM IR support for scripts

--- compiler/tests/org/jetbrains/kotlin/generators/tests/GenerateCompilerTests.kt
+++ compiler/tests/org/jetbrains/kotlin/generators/tests/GenerateCompilerTests.kt
@@ -525,6 +525,10 @@ fun main(args: Array<String>) {
             testClass<AbstractIrAsmLikeInstructionListingTest> {
                 model("codegen/asmLike", targetBackend = TargetBackend.JVM_IR)
             }
+
+            testClass<AbstractIrScriptCodegenTest> {
+                model("codegen/script", extension = "kts", targetBackend = TargetBackend.JVM_IR)
+            }
         }
 
         testGroup(
@@ -534,6 +538,10 @@ fun main(args: Array<String>) {
         ) {
             testClass<AbstractFirBlackBoxCodegenTest> {
                 model("codegen/box", targetBackend = TargetBackend.JVM_IR, excludeDirs = listOf("oldLanguageVersions"))
+            }
+
+            testClass<AbstractFirScriptCodegenTest> {
+                model("codegen/script", extension = "kts", targetBackend = TargetBackend.JVM_IR, excludeDirs = listOf("oldLanguageVersions"))
             }
 
             testClass<AbstractFirBlackBoxInlineCodegenTest> {
